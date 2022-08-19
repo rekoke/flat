0f22967140546d81c3abe86da7a538424bc4eb2c
koke <rekoke92@gmail.com>
2022-08-19 21:25:50 +0200

    eslint added and code fixed

--- flat_front/src/App.js
+++ flat_front/src/App.js
@@ -1,28 +1,29 @@
-import { useEffect, useState } from 'react'
+import {useEffect, useState} from 'react';
 
 function App() {
-  const [products, setProducts] = useState([])
-  const [name, setName] = useState("");
+  const [products, setProducts] = useState([]);
+  const [name, setName] = useState('');
   const [price, setPrice] = useState(0);
   const [fav, setFav] = useState(false);
-  const [message, setMessage] = useState("");
+  const [message, setMessage] = useState('');
 
-  useEffect (() => {
+  useEffect(() => {
     fetch('http://localhost:4000/products')
-    .then(res => res.json())
-    .then(data => setProducts(data))
+        .then((res) => res.json())
+        .then((data) => setProducts(data));
   }, []);
 
   const handleOnChange = () => {
     setFav(!fav);
   };
-  let handleSubmit = async (e) => {
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       await fetch('http://localhost:4000/products', {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify({
           id: 0,
@@ -31,18 +32,18 @@ function App() {
           is_fav: fav,
         }),
       }).then((res) => {
-        if(res.ok){
-          return res.json()
+        if (res.ok) {
+          return res.json();
         }
         throw new Error('Something went wrong');
       })
-      .then((data) => {
-        setName("");
-        setPrice("");
-        setFav(false);
-        setMessage("User created successfully");
-        setProducts(data)
-      });
+          .then((data) => {
+            setName('');
+            setPrice('');
+            setFav(false);
+            setMessage('User created successfully');
+            setProducts(data);
+          });
     } catch (err) {
       console.log(err);
     }
@@ -52,36 +53,37 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1> all productos</h1>
-        {products && products.map(product => (<div key={product.id}>{product.name}-{product.price}-{product.is_fav ? 'true' : 'false'}</div>))}
+        {products && products.map((product) => (
+          <div key={product.id}>
+            {product.name}-{product.price}-{product.is_fav ? 'true' : 'false'}
+          </div>
+        ))}
       </header>
       <main>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={name}
-          placeholder="Name"
-          onChange={(e) => setName(e.target.value)}
-        />
-        <input
-          type="number"
-          value={price}
-          placeholder="price"
-          onChange={(e) => setPrice(e.target.value)}
-        />
-        <label htmlFor="isfav">is fav</label>
-        <input 
-          type="checkbox"
-          id="isfav"
-          name="isfav"
-          checked={fav}
-          onChange={handleOnChange}
-
-        />
-
-        <button type="submit">Create mf</button>
-
-        <div className="message">{message ? <p>{message}</p> : null}</div>
-      </form>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={name}
+            placeholder="Name"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="number"
+            value={price}
+            placeholder="price"
+            onChange={(e) => setPrice(e.target.value)}
+          />
+          <label htmlFor="isfav">is fav</label>
+          <input
+            type="checkbox"
+            id="isfav"
+            name="isfav"
+            checked={fav}
+            onChange={handleOnChange}
+          />
+          <button type="submit">Create mf</button>
+          <div className="message">{message ? <p>{message}</p> : null}</div>
+        </form>
       </main>
     </div>
   );
